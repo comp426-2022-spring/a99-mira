453c029f7eed9b5a5cd4dce89bfcac90130e42c9
liyachn <liyachen@live.unc.edu>
2022-04-27 13:15:57 -0400

    active div function

---
+++ public/script.js
@@ -0,0 +1,8 @@
+function div(name) { // set active div on click
+    var active = Array.from(document.getElementsByClassName("active")) // get collection of active divs
+    active.forEach(function (currentdiv) {
+        currentdiv.setAttribute("class", "hidden");
+    })
+
+    document.getElementById(name).setAttribute("class", "active") // show clicked div
+}
